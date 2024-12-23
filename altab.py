4c442446d435c93a3562831f3bc35de8c406376b
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-12-23 02:06:11 +0200

    clean up

--- altab.py
+++ altab.py
@@ -7,13 +7,12 @@ def main():
     i3 = i3ipc.Connection()
 
     with open('/tmp/last_focus.json', 'r') as f:
-        last_focus_by_monitor = json.load(f)
+        last_focused_by_monitor = json.load(f)
+        current_focused_monitor = [w for w in i3.get_workspaces() if w.focused][0].output
 
-    focused_monitor = [w for w in i3.get_workspaces() if w.focused][0].output
-
-    if focused_monitor in last_focus_by_monitor:
-        last_workspace_id = last_focus_by_monitor[focused_monitor]
-        i3.command(f"workspace {last_workspace_id}")
+        if current_focused_monitor in last_focused_by_monitor:
+            last_workspace_id = last_focused_by_monitor[current_focused_monitor]
+            i3.command(f"workspace {last_workspace_id}")
 
 if __name__ == '__main__':
     main()
--- daemon.py
+++ daemon.py
@@ -8,23 +8,23 @@ last_monitor = ""
 
 def on_workspace_focus(i3, e):
     global last_monitor
-    last_workspace = e.old.name
-    output = [w for w in i3.get_workspaces() if w.focused][0].output
-    if last_monitor == output:
-        update_focus(last_workspace, output)
-    last_monitor = output
 
-def update_focus(last_workspace, output):
-    last_focused_by_monitor[output] = last_workspace
+    last_workspace = e.old.name
+    monitor = [w for w in i3.get_workspaces() if w.focused][0].output
+
+    if monitor == last_monitor:
+        update_focus(last_workspace, monitor)
+    last_monitor = monitor
+
+def update_focus(last_workspace, monitor):
+    last_focused_by_monitor[monitor] = last_workspace
 
     with open("/tmp/last_focus.json", 'w') as f:
         json.dump(last_focused_by_monitor, f)
 
 def main():
     i3 = i3ipc.Connection()
-
     i3.on("workspace::focus", on_workspace_focus) 
-
     i3.main()
 
 if __name__ == "__main__":
